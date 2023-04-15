b130b5f098ea57ead075b9a4e04f8516cf4833d7
Tanvi <tanvigadgil2000@gmail.com>
2023-04-14 18:42:37 -0700

    Add testcases

--- HW3/homework.py
+++ HW3/homework.py
@@ -1,35 +1,72 @@
-def convertToCNF(sentence):
-    index = sentence.find("=>")
+# def createClauses(sentence):
+#     args = []
+    
 
-    if index == -1:
-        pass
-    else:
-        predicate = sentence[:index]
-        conclusion = sentence[index + 2:]
-        # TODO: Do I need to strip the spaces from the predicate and conclusion?
-        # TODO: Negation operations on predicate
+# def convertToCNF(sentence):
+#     implicationIndex = sentence.find("=>")
+
+#     if implicationIndex == -1:
+#         pass
+#     else:
+#         premise = sentence[:implicationIndex]
+#         conclusion = sentence[implicationIndex + 2:]
+#         # TODO: Do I need to strip the spaces from the predicate and conclusion?
+
+#         andIndex = premise.find("&")
+
+#         while(andIndex != -1):
+#             pass
+#         # TODO: Negation operations on predicate
         
-        fact = predicate + '|' + conclusion
+#         fact = premise + '|' + conclusion
 
-        print(fact)
-        return fact
+#         print(fact)
+#         return fact
 
+class Sentence:
+    def __init__(self):
+        pass
+
+    def makeSentence(sentence):
+        pass
+
+    def printSentences(sentences):
+        for sentence in sentences:
+            print(sentence)
+
+
+    
+class FOLResolution:
+    def __init__(self):
+        self.nOfKB = 0
+        self.KB = list()
+        self.Query = list()
+
+    # Read input file and process it
+    def processInput(self, inputFile):
+        file = open(inputFile, "r")
+        Lines = file.readlines()
+
+        # Query
+        query = Lines[0].strip().replace(" ", "")
+        self.Query.append(Sentence.makeSentence(query))
+        print("Query: ")
+        Sentence.printSentences(self.Query)
+
+        # Number of sentences given in the KB
+        self.nOfKB = int(Lines[1])
+        print(self.nOfKB)
+
+        # List of sentences in the KB
+        for i in range(2, self.nOfKB + 2):
+            sentence = Lines[i].strip().replace(" ", "")
+            self.KB.append(Sentence.makeSentence(sentence))
+        print("KB: ")
+        Sentence.printSentences(self.KB)
+
+def main():
+    fol = FOLResolution()
+    fol.processInput('input.txt')
 
 if __name__ == "__main__":
-    # Read input file
-    file = open('input1.txt', 'r')
-    Lines = file.readlines()
-
-    query = Lines[0]
-    print(query)
-    noOfSentences = int(Lines[1])
-    print(noOfSentences)
-
-    # Create a list of sentences
-    listOfSentences = list()
-    for i in range(2, noOfSentences + 2):
-        listOfSentences.append(Lines[i].strip('\n'))
-    print(listOfSentences)
-
-    for sentence in listOfSentences:
-        convertToCNF(sentence)
+    main()
