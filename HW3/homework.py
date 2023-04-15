6a1fd42becae804cd655e438920db861c3e55eec
Tanvi <tanvigadgil2000@gmail.com>
2023-04-14 21:32:28 -0700

    Create predicates

--- HW3/homework.py
+++ HW3/homework.py
@@ -1,3 +1,5 @@
+import copy
+
 # def createClauses(sentence):
 #     args = []
     
@@ -23,13 +25,76 @@
 #         print(fact)
 #         return fact
 
+
+class Predicate:
+    def __init__(self, name, negationFlag, args):
+        self.name = name
+        self.negationFlag = negationFlag
+        self.args = args
+
+    def negate(self):
+        self.negationFlag = not self.negationFlag
+
+    def isVariable(variable):
+        if variable[0].islower():
+            return True
+        return False
+
+    def makePredicate(predicateString):
+        negationFlag = False
+        predicateString = predicateString[:-1]
+        if predicateString[0] == '~':
+            negationFlag = True
+            predicateString = predicateString[1:]
+        
+        splitString = predicateString.split("(")
+        name = splitString[0]
+        args = splitString[1].split(",")
+
+        for i in range(len(args)):
+            if Predicate.isVariable(args[i]):
+                if args[i] in Sentence.variableMap:
+                    args[i] = Sentence.variableMap[args[i]]
+                else:
+                    Sentence.variableMap[args[i]] = "var" + str(Sentence.variableCounter)
+                    args[i] = Sentence.variableMap[args[i]]
+                    Sentence.variableCounter += 1
+            print(args[i])
+
+        return Predicate(name, negationFlag, args)
+
 class Sentence:
-    def __init__(self):
-        pass
+    variableMap = dict()
+    variableCounter = 0
 
-    def makeSentence(sentence):
-        pass
+    def __init__(self, predicates):
+        self.predicates = predicates
 
+    # Make a sentence from a string
+    def makeSentence(sentenceString):
+        predicates = list()
+
+        if "=>" in sentenceString:
+            # List of predicates splited by "=>"
+            splitSpring = sentenceString.split("=>")
+            print(splitSpring)
+
+            # TODO: Check for OR as well
+            premise = splitSpring[0].split("&")
+
+            for each in premise:
+                p = Predicate.makePredicate(each)
+                p.negate()
+                predicates.append(p)
+            conclusion = Predicate.makePredicate(splitSpring[1])
+            predicates.append(conclusion)
+
+        else:
+            predicates.append(Predicate.makePredicate(sentenceString))
+        Sentence.variableMap.clear()
+        return Sentence(predicates)
+
+    # Print each sentence in the list
     def printSentences(sentences):
         for sentence in sentences:
             print(sentence)
@@ -64,9 +129,25 @@ class FOLResolution:
         print("KB: ")
         Sentence.printSentences(self.KB)
 
+    def writeOutput(self):
+        outputFile = open("output.txt", "w")
+        KBToAsk = copy.copy(self.KB)
+        result = self.ask(KBToAsk, self.Query)
+        print(result)
+
+        if result:
+            outputFile.write("TRUE")
+        else:
+            outputFile.write("FALSE")
+
+    def ask(self, KB, query):
+        pass
+
+
 def main():
     fol = FOLResolution()
     fol.processInput('input.txt')
+    fol.writeOutput()
 
 if __name__ == "__main__":
     main()
