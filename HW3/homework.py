4c93adbc033a53102e31a52db8af9d48053a8255
Tanvi <tanvigadgil2000@gmail.com>
2023-04-18 01:10:25 -0700

    Add resolve function

--- HW3/homework.py
+++ HW3/homework.py
@@ -27,6 +27,7 @@ import copy
 
 
 class Predicate:
+    # Format: [Name, True, [arg1, arg2, ...]]
     def __init__(self, name, negationFlag, args):
         self.name = name
         self.negationFlag = negationFlag
@@ -40,15 +41,34 @@ class Predicate:
         arguments = ','.join(self.args)
         return negation + self.name + "(" + arguments + ")"
 
+    # Overloading the equal function
+    def __eq__(self, other):
+        if self.negationFlag != other.negationFlag:
+            return False
+        
+        if self.name != other.name:
+            return False
+        
+        if len(self.args) != len(other.args):
+            return False
+        
+        for i in range(len(self.args)):
+            if (not Predicate.isVariable(self.args[i])) and (not Predicate.isVariable(other.args[i])) and self.args[i] != other.args[i]:
+                return False
+        return True
+
+    # Change the negation flag
     def negate(self):
         self.negationFlag = not self.negationFlag
 
+    # Check if the argument is a variable
     def isVariable(variable):
         if variable[0].islower():
             return True
         return False
 
-    def makePredicate(predicateString):
+    # Make a predicate from a string
+    def createPredicate(predicateString):
         negationFlag = False
         predicateString = predicateString[:-1]
         if predicateString[0] == '~':
@@ -71,10 +91,60 @@ class Predicate:
 
         return Predicate(name, negationFlag, args)
 
+    # Check if two predicates can be unified
+    def canBeUnified(self, factPredicate):
+        for i in range(len(self.args)):
+            if (not Predicate.isVariable(self.args[i])) and (not Predicate.isVariable(factPredicate.args[i])) and (self.args[i] != factPredicate.args[i]):
+                return False
+        return True
+
+    # Check if two predicates can be canceled
+    def canBeCanceled(self, factPredicate):
+        if self.name == factPredicate.name and self.negationFlag != factPredicate.negationFlag:
+            if self.canBeUnified(factPredicate):
+                return True
+            return False
+
+    # Unify two predicates
+    def unify(predicate1, predicate2):
+        unificationMap = dict()
+        isUnified = False
+
+        for i in range(len(predicate1.args)):
+            # If both are variables
+            if Predicate.isVariable(predicate1.args[i]) and Predicate.isVariable(predicate2.args[i]):
+                unificationMap[predicate1.args[i]] = predicate2.args[i]
+                
+            # If first is constant and second is variable
+            elif not Predicate.isVariable(predicate1.args[i]) and Predicate.isVariable(predicate2.args[i]):
+                unificationMap[predicate2.args[i]] = predicate1.args[i]
+                isUnified = True
+
+            # If first is variable and second is constant
+            elif Predicate.isVariable(predicate1.args[i]) and not Predicate.isVariable(predicate2.args[i]):
+                unificationMap[predicate1.args[i]] = predicate2.args[i]
+                isUnified = True
+
+            # If both are constants and arguments are equal
+            elif not Predicate.isVariable(predicate1.args[i]) and not Predicate.isVariable(predicate2.args[i]) and predicate1.args[i] == predicate2.args[i]:
+                unificationMap[predicate1.args[i]] = predicate2.args[i]
+                isUnified = True
+
+            # If both are constants and arguments are not equal
+            elif not Predicate.isVariable(predicate1.args[i]) and not Predicate.isVariable(predicate2.args[i]) and predicate1.args[i] != predicate2.args[i]:
+                isUnified = False
+                break
+
+        if not isUnified:
+            unificationMap.clear()
+
+        return unificationMap
+
 class Sentence:
     variableMap = dict()
     variableCounter = 0
 
+    # Format: [predicate1, predicate2, ...]
     def __init__(self, predicates):
         self.predicates = predicates
 
@@ -86,17 +156,13 @@ class Sentence:
         sentence = ','.join(predicateString)
         return "[" + sentence + "]"
 
-    # Print each sentence in the list
-    def printSentences(sentences):
-        for sentence in sentences:
-            print(sentence)
-
+    # Negate the sentence by negating each predicate
     def negateSentence(self):
-        for predicate in self.predicates:
-            predicate.negate()
+        for i in range(len(self.predicates)):
+            self.predicates[i].negate()
 
     # Make a sentence from a string
-    def makeSentence(sentenceString):
+    def createSentence(sentenceString):
         predicates = list()
 
         if "=>" in sentenceString:
@@ -108,20 +174,62 @@ class Sentence:
             premise = splitSpring[0].split("&")
 
             for each in premise:
-                p = Predicate.makePredicate(each)
+                p = Predicate.createPredicate(each)
                 p.negate()
                 predicates.append(p)
-            conclusion = Predicate.makePredicate(splitSpring[1])
+            conclusion = Predicate.createPredicate(splitSpring[1])
             predicates.append(conclusion)
 
         else:
-            predicates.append(Predicate.makePredicate(sentenceString))
+            predicates.append(Predicate.createPredicate(sentenceString))
         Sentence.variableMap.clear()
         return Sentence(predicates)
 
-    def getPossibleClauses(self):
-        pass
+    # Check if the sentence can be resolved
+    def canBeResolved(self, fact):
+        for predicate in self.predicates:
+            for factPredicate in fact.predicates:
+                if predicate.canBeCanceled(factPredicate):
+                    return True
+        return False
+
+    # Get all the possible clauses that can be resolved
+    def getPossibleClauses(self, kb):
+        clauses = list()
+        for fact in kb:
+            if self.canBeResolved(fact):
+                clauses.append(fact)
+        return clauses
+
+    # Get all the possible substitutions
+    def getPossibleSubstitutions(self, clause):
+        substitutes = list()
+        for predicate in self.predicates:
+            for clausePredicate in clause.predicates:
+                if predicate.name == clausePredicate.name:
+                    unificationMap = Predicate.unify(predicate, clausePredicate)
+                    if len(unificationMap) != 0:
+                        substitutes.append(unificationMap)
+        return substitutes
     
+    # Substitute the variables in the sentence
+    def substitute(predicate1, predicate2, substitution):
+        inference = list()
+
+        return Sentence(inference)
+
+    # Resolve the sentence with the given clause
+    def resolve(self, clause):
+        inferences = list()
+        substitutions = self.getPossibleSubstitutions(clause)
+        print(substitutions)
+        for substitution in substitutions:
+            substitute = Sentence.substitute(self, clause, substitution)
+            print(substitute)
+            inferences.append(substitute)
+            print(inferences)
+        return inferences
+
 class FOLResolution:
     def __init__(self):
         self.nOfKB = 0
@@ -140,7 +248,7 @@ class FOLResolution:
 
         # Query
         query = Lines[0].strip().replace(" ", "")
-        self.Query.append(Sentence.makeSentence(query))
+        self.Query.append(Sentence.createSentence(query))
         print("Query: ")
         FOLResolution.printSentences(self.Query)
 
@@ -151,10 +259,11 @@ class FOLResolution:
         # List of sentences in the KB
         for i in range(2, self.nOfKB + 2):
             sentence = Lines[i].strip().replace(" ", "")
-            self.KB.append(Sentence.makeSentence(sentence))
+            self.KB.append(Sentence.createSentence(sentence))
         print("KB: ")
         FOLResolution.printSentences(self.KB)
 
+    # Write the output to the output file
     def writeOutput(self):
         outputFile = open("output.txt", "w")
         KBToAsk = copy.copy(self.KB)
@@ -166,35 +275,58 @@ class FOLResolution:
         else:
             outputFile.write("FALSE")
 
+    # Remove duplicates from the list
+    def removeDuplicates(self, kb):
+        toBeRemoved = list()
+        withNoDuplicates = list()
+        return withNoDuplicates
+
+    # Difference between two lists
+    def difference(self, kb1, kb2):
+        differenceList = list()
+
+        return differenceList
+
+    # Ask the query
     def ask(self, kb, query):
         queryAsked = copy.copy(query)
         print("Query asked: ")
         print(queryAsked)
         queryAsked.negateSentence()
         kb.append(queryAsked)
+        FOLResolution.printSentences(kb)
         kb.sort(key = lambda x: len(x.predicates))
+        print("Sorted KB: ")
+        FOLResolution.printSentences(kb)
 
         while True:
             newKB = list()
-            for sentence in kb:
-                clauses = sentence.getPossibleClauses()
+            for fact in kb:
+                print("Fact: ")
+                print(fact)
+                clauses = fact.getPossibleClauses(kb)
                 for clause in clauses:
-                    resolvents = sentence.resolve(clause)
+                    print(fact, clause)
+                    resolvents = fact.resolve(clause)
                     for resolvent in resolvents:
                         if len(resolvent.predicates) == 1 and resolvent.predicates[0].name == "TRUE":
                             return True
                     
                     newKB += resolvents
+
             kbWithRemovedDuplicates = self.removeDuplicates(newKB)
+            FOLResolution.printSentences(kbWithRemovedDuplicates)
             if len(kbWithRemovedDuplicates) == 0:
                 break
+
             differenceBtwKB = self.difference(kb, kbWithRemovedDuplicates)
-
-            if len(kbWithRemovedDuplicates) == 0:
+            FOLResolution.printSentences(differenceBtwKB)
+            if len(differenceBtwKB) == 0:
                 break
 
-            kb = kbWithRemovedDuplicates
+            kb += differenceBtwKB
             kb.sort(key = lambda x: len(x.predicates))
+            FOLResolution.printSentences(kb)
 
         return False
 
